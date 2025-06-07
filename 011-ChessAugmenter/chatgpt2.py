901113de63e581f822dc0abe535cf8edb042e39c
Christer Nilsson <janchrister.nilsson@gmail.com>
2025-06-07 16:29:02 +0200

    x

--- 011-ChessAugmenter/chatgpt2.py
+++ 011-ChessAugmenter/chatgpt2.py
@@ -3,20 +3,27 @@ import chess.engine
 import chess
 import os
 import time
-from urllib.parse import quote, unquote
+from urllib.parse import quote
 
-TIME = 0.1
+VIEWER = "https://christernilsson.github.io/2025/012-ChessViewer/"
+
+TIME = 1
 MPV = 5
 
-# HEADERS = "Date White WhiteElo Black BlackElo Result TimeControl ChapterURL".split(" ")
 STOCKFISH_PATH = "C:\\Program Files\\stockfish\\stockfish-windows-x86-64-avx2.exe"
 
-def centipawn(score):
-    if score.is_mate():
-        return 100000 if score.mate() > 0 else -100000
-    return score.score()
+def eval_board(engine, board, time_limit=TIME):
+    info = engine.analyse(board, chess.engine.Limit(time=time_limit),multipv=MPV)[0]
+    score = info["score"].white()
+    return score.score(mate_score=100000)
 
 def process(pgnfile):
+
+    def header(name):
+        if name in game.headers:
+            return game.headers[name]
+        else:
+            return ''
 
     with open('pgn/' + pgnfile, encoding='utf8') as pgn:
         game = chess.pgn.read_game(pgn)
@@ -26,71 +33,68 @@ def process(pgnfile):
 
     board = game.board()
 
-    analys = []
-    evalueringar = []
-    moves = list(game.mainline_moves())
+    movesx = list(game.mainline_moves())
 
-    print(f"{filename} {len(moves)}",'', end='')
-    for i in range(len(moves)):
-        move = moves[i]
+    moves = []
+    evals = []
+    bests = []
+
+    print(f"{filename} {len(movesx)}",'', end='')
+    for move in movesx:
+
         print('.', end='')
-        info = engine.analyse(board, chess.engine.Limit(time=TIME), multipv=MPV)[0]
-        evalueringar.append([info["score"], info["pv"][0]])  # score & bästa drag
-        board.push(move)
-    print()
 
-    info = engine.analyse(board, chess.engine.Limit(time=TIME), multipv=MPV)[0]
-    if "pv" in info:
-        evalueringar.append([info["score"], info["pv"][0]])  # score & bästa drag
-    else:
-        evalueringar.append([info["score"], move])
+        best_info = engine.analyse(board, chess.engine.Limit(time=TIME), multipv=MPV)
+        best_score = best_info[0]["score"].white().score(mate_score=100000)
+        best_move = best_info[0]["pv"][0]  # Första draget i bästa variant
+
+        played_move = move
+        best_san = board.san(best_move)
+        played_san = board.san(played_move)
+        board.push(played_move)
+        actual_score = eval_board(engine, board, TIME)
+        loss = best_score - actual_score
+
+        moves.append(played_san)
+        evals.append(str(loss))
+        bests.append(best_san)
 
     engine.quit()
 
-    board = game.board()
-    moves = list(game.mainline_moves())
-    for i in range(len(moves)):
-        if i+1 >= len(evalueringar): continue
-        move = moves[i]
-        played = move
-        played_san = board.san(played)
-        best = evalueringar[i][1]
-        best_san = board.san(best)
+    print()
 
-        score_before = centipawn(evalueringar[i][0].white() if board.turn == chess.WHITE else evalueringar[i][0].black())
-        score_after = centipawn(evalueringar[i+1][0].white() if board.turn == chess.WHITE else evalueringar[i+1][0].black())
-
-        analys.append([played_san,score_before,best_san,score_after])
-        board.push(played)
-
-    with open('url/' + pgnfile.replace('.pgn','.txt'), "w", encoding="utf-8") as url:
+    with open(year + '/' + pgnfile.replace('.pgn','.txt'), "w", encoding="utf-8") as url:
 
         headers = []
-        moves = []
-        evals = []
-        bests = []
+        headers.append(f"Date={header('Date')} Result:{header('Result')}")
+        headers.append(f"White={header('WhiteElo')} {header('White')}")
+        headers.append(f"Black={header('BlackElo')} {header('Black')}")
+        headers.append(f'Link={header('ChapterURL')}')
+        headers.append(f'Seek=TIME:{TIME} MPV:{MPV}')
 
-        for term in game.headers:
-            headers.append(term + "=" + quote(game.headers[term]))
-
-        headers.append(f'Seek= TIME={TIME} MPV={MPV}')
-
-        for i in range(len(analys)):
-            curr = analys[i]
-            moves.append(curr[0].ljust(0))
-            evals.append(str(curr[1]).ljust(0))
-            bests.append(curr[2].ljust(0))
 
         headers = '&'.join(headers)
         moves = quote('_'.join(moves))
         evals = quote('_'.join(evals))
         bests = quote('_'.join(bests))
 
-        url.write('http://127.0.0.1:5500/index.html?' + headers + '&move=' + moves + '&eval=' + evals + '&best=' + bests + '\n')
+        url.write(VIEWER + "index.html?" + headers.replace(" ","_") + '&move=' + moves + '&eval=' + evals + '&best=' + bests + '\n')
 
 # Gå igenom alla filer i aktuell katalog
 start = time.time()
 for filename in os.listdir('pgn'):
-    url_file = os.path.splitext('url/' + filename)[0] + '.txt'
-    if not os.path.exists(url_file): process(filename)
+    year = filename[0:4]
+    year_file = year + "\\" + filename.replace('.pgn','.txt')
+    if not os.path.exists(year_file): process(filename)
+
+data = []
+YEAR = 2025
+with open(f"{YEAR}/_index.md", 'w', encoding="utf-8") as md:
+    md.write(f"---\ntitle: {YEAR}\nauto: true\n---\n")
+    for filename in os.listdir(str(YEAR)):
+        # if filename[0:4] != '2025': continue
+        if not filename.endswith('.txt'): continue
+        with open(f"{YEAR}/{filename}", encoding="utf-8") as url:
+            md.write(f"[{filename.replace('.txt','')}]({url.read()})  \n")
+
 print(time.time() - start)
