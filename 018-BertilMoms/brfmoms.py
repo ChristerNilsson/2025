1b027868868410fa06e5bde4e2b2cbb778760c7d
Christer Nilsson <janchrister.nilsson@gmail.com>
2025-08-17 09:14:44 +0200

    OOP

--- 018-BertilMoms/brfmoms.py
+++ 018-BertilMoms/brfmoms.py
@@ -2,9 +2,9 @@ import re
 import time
 from pathlib import Path
 
-#SIE_FIL = "202206.sie4"
+SIE_FIL = "202206.sie4"
 #SIE_FIL = "2022.sie4"
-SIE_FIL = "2023.sie4"
+#SIE_FIL = "2023.sie4"
 
 UNDRE_MOMS_ANDEL = 13 # %
 ÖVRE_MOMS_ANDEL = 16 # %
@@ -20,6 +20,25 @@ FINANSIELLA_OCH_ÖVRIGA_INTÄKTER_O_KOSTNADER = '8'
 
 VER_RE = re.compile(r'#VER\s+"(?P<serie>\d+)"\s+"(?P<id>\d+)"\s+(?P<datum>\d{8})\s+"(?P<text>[^"]*)"')
 TRANS_RE = re.compile(r'#TRANS\s+(?P<konto>\d+)\s+{}\s+(?P<belopp>[-+]?\d+(?:[.,]\d+)?)')
+
+class Verifikat:
+	def __init__(s,serie,id,datum,text):
+		s.serie = serie
+		s.id = id
+		s.datum = datum
+		s.text = text
+		s.transaktioner = []
+	def addTransaktion(s,konto,belopp):
+		s.transaktioner.append(Transaktion(konto,belopp))
+	def __str__(s):
+		return f"{s.serie} {s.id} {s.datum} {s.text}"
+
+class Transaktion:
+	def __init__(s,konto,belopp):
+		s.konto = konto
+		s.belopp = belopp
+	def __str__(s):
+		return f"{s.konto} {s.belopp:.2f} {konton[s.konto]}"
 
 ignorerade = []
 
@@ -44,7 +63,7 @@ def read_sie(infile):
 		if line.startswith("#VER"):
 			m = VER_RE.match(line)
 			if not m: raise ValueError(f"Kunde inte tolka VER-raden: {line}")
-			verifikat = {"serie": m.group("serie"),"id": m.group("id"),"datum": m.group("datum"),"text": m.group("text"),"transaktioner": []}
+			verifikat = Verifikat(m.group("serie"), m.group("id"), m.group("datum"), m.group("text"))
 			continue
 
 		if line == "{":
@@ -63,36 +82,30 @@ def read_sie(infile):
 			tm = TRANS_RE.match(line)
 			if not tm: raise ValueError(f"Kunde inte tolka TRANS-rad: {line}")
 			konto = tm.group("konto")
-			belopp = float(tm.group("belopp").replace(",", "."))
-			verifikat["transaktioner"].append({"konto": konto, "belopp": belopp})
+			belopp = float(tm.group("belopp"))
+			verifikat.addTransaktion(konto, belopp)
 
 	return konton,verifikationer
 
 start = time.time()
 konton,verifikationer = read_sie(SIE_FIL)
-filtrerade = []
-
-for verifikat in verifikationer:
-	for transaktion in verifikat["transaktioner"]:
-		konto = transaktion["konto"]
-		belopp = transaktion["belopp"]
-		if konto == MOMS_KONTO and belopp != 0: filtrerade.append(verifikat)
+filtrerade = [v for v in verifikationer if any([t.konto == MOMS_KONTO and t.belopp != 0 for t in v.transaktioner])]
 
 summaUtgiftSomBerörs = 0 # ören
 for verifikat in filtrerade:
 	ingåendeMoms = 0 # ören
 	kontonPlus = 0 # ören
-	print(verifikat["serie"], verifikat["id"], verifikat["datum"], verifikat["text"])
-	for transaktion in verifikat["transaktioner"]:
-		konto = transaktion["konto"]
-		belopp = 100 * transaktion["belopp"] # pga avrundningsfel i python räknas i ören
+	print(verifikat)
+	for transaktion in verifikat.transaktioner:
+		konto = transaktion.konto
+		belopp = 100 * transaktion.belopp # pga avrundningsfel i python räknas i ören
 		if konto == MOMS_KONTO: ingåendeMoms += belopp
 		if konto[0] != EGET_KAPITAL_OCH_SKULDER: kontonPlus += belopp
-		if belopp != 0: print('  ',konto,f"{belopp/100:.2f}", konton[konto])
+		if belopp != 0: print('  ',transaktion) #f"{belopp/100:.2f}", konton[konto])
 
 	summaUtgiftInklMoms = kontonPlus + ingåendeMoms # ören
 	if summaUtgiftInklMoms == 0:
-		ignorerade.append(verifikat["id"])
+		ignorerade.append(verifikat.id)
 		print("   *** DIV MED NOLL",summaUtgiftInklMoms)
 	else:
 		momsAndel = ingåendeMoms / summaUtgiftInklMoms / 0.2 * 100
